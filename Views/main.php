8de534663d2ffc99b9b38e00dabfadba3cf8e404
Manuel Maisano <manumaisano@gmail.com>
2024-05-07 16:12:42 +0200

    table hotels

---
+++ Views/main.php
@@ -0,0 +1,24 @@
+<?php
+$template = "";
+foreach ($hotels as $key) {
+    $template .= "<tr> <td>{$key['name']}</td> <td>{$key['description']}</td> <td>{$key['parking']}</td> <td>{$key['vote']}</td> <td>{$key['distance_to_center']}</td> </tr>";
+};
+?>
+
+
+<main>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Parking</th>
+                <th scope="col">Vote</th>
+                <th scope="col">Distance</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php echo $template ?>
+        </tbody>
+    </table>
+</main>
--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 include __DIR__ . "/Models/hotels.php";
-var_dump($hotels);
+//var_dump($hotels);
 
-
-
-
+include __DIR__ . "/Views/header.php";
 
 ?>
+<?php
+include __DIR__ . "/Views/main.php";
+?>
+
