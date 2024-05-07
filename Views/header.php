7a99f64230ac2e4ff57c1af2ca32f179b2c5d569
Manuel Maisano <manumaisano@gmail.com>
2024-05-07 16:07:45 +0200

    header php

---
+++ Views/header.php
@@ -0,0 +1,32 @@
+
+<?php
+
+//Descrizione
+//Partiamo da questo array di hotel. https://www.codepile.net/pile/OEWY7Q1G
+//Stampare tutti i nostri hotel con tutti i dati disponibili.
+//Iniziate in modo graduale.
+//Prima stampate in pagina i dati, senza preoccuparvi dello stile.
+//Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella.
+
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Hotels</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Hotels</h1>
+    </div>
+    
+</body>
+</html>
